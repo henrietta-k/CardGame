42f121986b5638e6351ecf2bea78cb0d4df1b3f4
henrietta-k <111554249+henrietta-k@users.noreply.github.com>
2023-06-26 16:46:26 +0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,36 +1,31 @@
 import java.util.*; 
 
-
 class Card {
-
-//creating variables 
+    
     public int rank;
     public int suit; 
 
-    //constructor
     public Card(int rank, int suit) {
         this.rank = rank;
         this.suit = suit; 
     }
 
-//Creates Strings using the ranks and suits 
-public String toString(){
-
+    //Creates Strings using the ranks and suits 
+    public String toString() {
         String[] ranks = {null,"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
         String[] suits = {"Clubs","Diamonds","Hearts","Spades"};    
-
+        
         String s = ranks[this.rank] + " of " + suits[this.suit];
         return s;
     }
-    
-    public int getRank() { //getter method
+
+    //Rank getter method
+    public int getRank() { 
         return this.rank;
     }
 
-    public int getSuit(){ //getter method 
+    //Suit getter method
+    public int getSuit(){ 
         return this.suit; 
-    }
-
-
-    
+    }   
 }
